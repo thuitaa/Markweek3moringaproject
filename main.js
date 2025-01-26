e329c85557e900d37af1adabeb21b1999023509d
Mark <markthuita08@gmail.com>
2025-01-26 19:45:52 +0300

    added condition to ensure that one fills in before submitting

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ document.getElementById("akan-name").addEventListener("submit", function (event)
     const fullName = document.getElementById("name").value.trim();
     const birthDate = document.getElementById("date-of-birth").value;
     const gender = document.querySelector('input[name ="gender"]:checked')?.id
+
+    if (!birthDate || !gender || !fullName) {
+        alert("Please fill in all the spaces before submitting")
+        return;
+    }
