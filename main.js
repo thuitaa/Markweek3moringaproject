3a3bba6a2b5c22a38118241d8855061de66416e7
Mark <markthuita08@gmail.com>
2025-01-26 19:39:15 +0300

    Adding user input

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 document.getElementById("akan-name").addEventListener("submit", function (event) {
     // Prevent the form from refreshing the page
     event.preventDefault();
-    
+
+// To obtain the user input
+    const fullName = document.getElementById("name").value.trim();
+    const birthDate = document.getElementById("date-of-birth").value;
+    const gender = document.querySelector('input[name ="gender"]:checked')?.id
