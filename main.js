e28ef82287c92929d89fd04fdb2fb8ccf848aa76
Mark <markthuita08@gmail.com>
2025-01-26 20:28:01 +0300

    final display of results

--- main.js
+++ main.js
@@ -14,3 +14,20 @@ document.getElementById("akan-name").addEventListener("submit", function (event)
 // to obtain the day of the week
     const date = new date(birthDate);
     const dayOfWeek = date.getDay(); // Sunday = 0, Monday = 1, ...
+
+    // linking the Akan name to the day of the week
+    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    let akanName;
+    if (gender === "male"){
+        akanName = maleNames[dayOfWeek];
+    }
+    elseif (gender === "female") {
+        akanName = femaleNames[dayOfWeek];
+    }
+
+ // Displaying the result
+    alert(`Hey ${fullName}! Based on your birthday, your Akan name is ${akanName}!`)
+
+});
