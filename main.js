54916ce9cf0dd69fae58030d3809fcf2f25f6273
Mark <markthuita08@gmail.com>
2025-01-27 23:29:04 +0300

    Applying the formula

--- main.js
+++ main.js
@@ -13,18 +13,32 @@ document.getElementById("akan-name").addEventListener("submit", function (event)
     }
 // to obtain the day of the week
     const date = new Date(birthDate);
-    const dayOfWeek = date.getDay();
+    const year = date.getFullYear();
+    const month = date.getMonth() +1; 
+    const day = date.getDate();
+
+// To obtain CC(century) and YY(last 2 digits)
+    const CC = Math.floor(year / 100); 
+    const YY = year % 100; // Year within the century
+
+// Applying formula
+    const dayWhenBorn = Math.floor (
+        (((4 * CC) - (2 * CC) - 1) + ((5 * YY) / 4) + ((26 * (month + 1)) / 10) + day) % 7
+    );
+
+    // To avoid negative values
+    const finalDay = (dayWhenBorn + 7) % 7
+
 
     // linking the Akan name to the day of the week
-    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-    let akanName;
-    if (gender === "male") {
-        akanName = maleNames[dayOfWeek];
-    } else if (gender === "female") {
-        akanName = femaleNames[dayOfWeek];
+    const akanNames = {
+    male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
+    female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
     }
+
+    const akanName = akanNames[finalDay][gender];
+
+
 
  // Displaying the result
     alert(`Hey ${fullName}! Based on your birthday, your Akan name is ${akanName}!`);
